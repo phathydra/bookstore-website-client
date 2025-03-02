fdc04dbaa56c62b873655ea80eb76b0c16788ad5
phathydra <toanphat124@gmail.com>
2025-03-02 22:05:21 +0700

    add items for products page

--- customers/src/pages/products/products.jsx
+++ customers/src/pages/products/products.jsx
@@ -1,28 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Categories from "../../components/categories/categories";
-import Product from "../../components/book/book";
-import data from "../../data.jsx";
+import Book from "../../components/book/book";
+import axios from "axios"
 import "./products.css";
 
 const Products = () => {
+    const [books, setBooks] = useState([])
+    const [page, setPage] = useState(0)
+    const [size, setSize] = useState(4)
+    const [pagesArr, setPagesArr] = useState([])
+
+    useEffect(() =>{
+        fetchBooks()
+    }, [page, size])
+
+    const fetchBooks = async() =>{
+        try {
+            const response = await axios.get("http://localhost:8081/api/book", {
+                params: { page, size }
+            });
+            console.log(response.data)
+            setBooks(response.data);
+            if(page === 0){
+                if(response.data.totalPages === 1){
+                    setPagesArr([0])
+                }
+                else {
+                    setPagesArr([0, 1])
+                }
+            }
+            else if(page === response.data.totalPages - 1){
+                setPagesArr([page - 1, page])
+            }
+            else{
+                setPagesArr([page - 1, page, page + 1])
+            }
+        } catch(e) {
+            console.error("Lỗi khi lấy danh sách sách:", e);
+        }
+    }
+
+    const handlePrevPage = () => {
+        setPage(page - 1)
+    }
+
+    const handleNextPage = () => {
+        setPage(page + 1)
+    }
+
+    const handleChangePage = (number) => {
+        setPage(number)
+    }
     
     return(
         <>
-            <div className="container">
+            <div className="products-page-container">
                 <div className="categories-container">
                     <Categories/>
                 </div>
                 <div className="products-container">
                     <div className="products">
-                        {data.products.map((product, index) => (
-                            <Product product={product} key = {index}/>
+                        {(books?.content || []).map((book, index) => (
+                            <Book book={book} key = {index}/>
                         ))}
                     </div>
                     <div className="page">
-                        <button> &lt; </button>
-                        <button> 1 </button>
-                        <button> 2 </button>
-                        <button> &gt; </button>
+                        <button disabled = {books.first} onClick = {handlePrevPage}> &lt; </button>
+                        {pagesArr.map((number, index) => (
+                            <button key={index} onClick={() => handleChangePage(number)} disabled={number === books.number}> {number + 1} </button>
+                        ))}
+                        <button disabled = {books.last} onClick = {handleNextPage}> &gt; </button>
                     </div>
                 </div>
             </div>
