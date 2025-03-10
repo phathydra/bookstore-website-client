24c3d0b2f54da6b771fc22004c1cd170f7ef27cb
phathydra <toanphat124@gmail.com>
2025-03-10 19:26:35 +0700

    add logic for button in header and fix search products error

--- customers/src/components/header/header.jsx
+++ customers/src/components/header/header.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./header.css";
 import logo from "../../assets/logo.png"; 
+import GUEST from "../../assets/GUEST.jpg";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [user, setUser] = useState({ name: "", avatar: "" });
   const [input, setInput] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
   const menuRef = useRef(null);
   const navigate = useNavigate();
 
@@ -23,6 +25,7 @@ const Header = () => {
           });
         })
         .catch((error) => console.error("Error fetching user data:", error));
+        setIsLogin(true);
     }
   }, []);
 
@@ -51,11 +54,11 @@ const Header = () => {
     window.location.reload();
   };
 
-  const handleProfileClick = () => navigate("/profile");
+  const handleProfileClick = () => isLogin ? navigate("/profile") : navigate("/login");
   const handleLogoClick = () => navigate("/");
-  const handleCartClick = () => navigate("/cart");
-  const handleAddressClick = () => navigate("/address");
-  const handlehistoryClick = () => navigate("/orderHistory");
+  const handleCartClick = () => isLogin ? navigate("/cart") : navigate("/login");
+  const handleAddressClick = () => isLogin ? navigate("/adress") : navigate("/login");
+  const handlehistoryClick = () => isLogin ? navigate("/orderHistory") : navigate("/login");
 
   return (
     <header className="header">
@@ -64,8 +67,8 @@ const Header = () => {
       </div>
 
       <nav className="nav-buttons">
-        <button className="nav-button">Home</button>
-        <button className="nav-button">Products</button>
+        <button className="nav-button" onClick={() => navigate("/")}>Home</button>
+        <button className="nav-button" onClick={() => navigate("/products?searchParam=")}>Products</button>
         <button className="nav-button">About</button>
       </nav>
 
@@ -75,7 +78,7 @@ const Header = () => {
       </div>
 
       <div className="avatar-container" ref={menuRef}>
-        <img className="avatar" src={user.avatar} alt="Profile" onClick={toggleMenu} />
+        <img className="avatar" src={isLogin ? user.avatar : GUEST} alt="Profile" onClick={toggleMenu} />
         <span className="username">{user.name}</span>
         {isMenuOpen && (
           <div className="avatar-menu">
